102cd17dcddad4befc7fda7ed1dc3d8ff11774b2
Maycol Sanchez Salazar <mayc1030@gmail.com>
2019-04-27 14:46:03 -0500

    update

--- js/ultimatepost/ultimatePostAjax.js
+++ js/ultimatepost/ultimatePostAjax.js
@@ -26,8 +26,8 @@ function getDataCallbackultimateposts(domainurl,categoria){
         var objc = JSON.parse(data);
 
         $.each(objc, function(key,value){
-                var indice = key+1;
-                $('<div class="carousel-item"><blockquote class="blockquote contentdrupalnotes"><img src="backend/'+ value.title+'" height="80" width="80" alt="Avatar" class="img-circle"> <p class="h3">'+value.title+'</p><a class="nav-link developments btn-drupalnotes btn-drupalnotes-outline" href="drupalnotes" role="button"><span class="icon-spinner5"></span><small>my</small> Leer Más</a></blockquote></div>').appendTo($("#carousel-proyects .carousel-inner"));
+                var indice = key;
+                $('<div class="carousel-item active"><blockquote class="blockquote contentdrupalnotes"><img src="img/myproyect1.jpg" height="80" width="80" alt="Avatar" class="img-circle"> <p class="h3">'+ value.title +'</p><footer>My Profile</footer></blockquote></div>').appendTo($("#carousel-proyects .carousel-inner"));
                 $('<li><img src="img/myproyect2.jpg" alt="Navigation avatar" data-target="#carousel-proyects" data-slide-to="'+ indice +'" class="img-fluid img-circle"></li>').appendTo($("#carousel-proyects .carousel-indicators"));
 
         })
