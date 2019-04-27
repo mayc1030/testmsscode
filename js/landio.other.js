450fa9a57c1248b4b228a14df798881cd7b43d13
Maycol Sanchez Salazar <mayc1030@gmail.com>
2019-04-27 18:32:06 -0500

    prueba hover

--- js/landio.other.js
+++ js/landio.other.js
@@ -29,7 +29,7 @@
     // Donut Chart
     // @codekit-prepend "plugins/chart.js";
 
-    var url_ultimatepost = "https://maycolsanchezsalazar.000webhostapp.com/cms/views/ajax/ultimatepost.php";
+
     var domainurl=  $(location).attr("href");
 
     const TIEMPO_INTERVALO_MILESIMAS_SEG = 2000;
@@ -104,63 +104,12 @@
 
     }
 
+
+    $.fn.getstopSlide =  function (){
         $(".carousel-inner .carousel-item .h3").hover(function(){
             clearInterval(intervalo);
         }, function(){
             intervalo = setInterval(changeView, TIEMPO_INTERVALO_MILESIMAS_SEG);
-        });
-
-    function getstopSlide() {
-        console.log("entro a funcin");
-        $(".carousel-inner .carousel-item .h3").hover(function(){
-            clearInterval(intervalo);
-        }, function(){
-            intervalo = setInterval(changeView, TIEMPO_INTERVALO_MILESIMAS_SEG);
-        });
-    }
-
-
-    //get_data_callback();
-// peticion ajax enviada como callback
-    function getDataCallbackultimateposts(domainurl,idselector){
-
-        var categoria = 0;
-
-        switch (idselector) {
-            case "proyects":
-                categoria = 1;
-                break;
-            case "contributions":
-                categoria = 2;
-                break;
-            case "developments":
-                categoria = 3;
-                break;
-        }
-
-        $.ajax({
-            data: { locationurl:domainurl,id:categoria},
-            dataType: "json",
-            type:"POST",
-            url:url_ultimatepost
-        }).done(function(data,textStatus,jqXHR){
-
-            var objc = JSON.parse(data);
-
-            $.each(objc, function(key,value){
-                var indice = key;
-                if ($('#carousel-'+ idselector +' .carousel-inner').find('div').length < objc.length) {
-                    $('<div class="carousel-item"><blockquote class="blockquote"><img src="img/myproyect1.jpg" height="80" width="80" alt="Avatar" class="img-circle"> <p class="h3">'+ value.title +'</p><footer>My Profile</footer></blockquote></div>').appendTo($("#carousel-"+ idselector +" .carousel-inner"));
-                    $('<li><img src="img/myproyect2.jpg" alt="Navigation avatar" data-target="#carousel-'+ idselector +'" data-slide-to="'+ indice +'" class="img-fluid img-circle"></li>').appendTo($("#carousel-"+ idselector +" .carousel-indicators"));
-                    $("#carousel-"+ idselector +" .carousel-inner div:first-child").addClass(" active");
-                }
-
-            });
-
-            getstopSlide();
-
-        }).fail(function(jqXHR,textStatus,textError){
-            alert("Error al realizar la peticion dame");
         });
     }
 
