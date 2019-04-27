c0d312060e73d472055590dadf8fa76cc35dacd9
Maycol Sanchez Salazar <mayc1030@gmail.com>
2019-04-27 02:09:40 -0500

    test

--- js/ultimatepost/ultimatePostAjax.js
+++ js/ultimatepost/ultimatePostAjax.js
@@ -27,7 +27,9 @@ function getDataCallbackultimateposts(domainurl,categoria){
 
         $.each(objc, function(key,value){
                 console.log( value.title);
-                $('<blockquote class="blockquote contentdrupalnotes"><img src="backend/'+ value.title+'" height="80" width="80" alt="Avatar" class="img-circle"> <p class="h3">'+value.title+'</p><a class="nav-link developments btn-drupalnotes btn-drupalnotes-outline" href="drupalnotes" role="button"><span class="icon-spinner5"></span><small>my</small> Leer Más</a></blockquote>').appendTo($("#carousel-proyects .carousel-inner .carousel-item"));
+                $('<div class="carousel-item"><blockquote class="blockquote contentdrupalnotes"><img src="backend/'+ value.title+'" height="80" width="80" alt="Avatar" class="img-circle"> <p class="h3">'+value.title+'</p><a class="nav-link developments btn-drupalnotes btn-drupalnotes-outline" href="drupalnotes" role="button"><span class="icon-spinner5"></span><small>my</small> Leer Más</a></blockquote></div>').appendTo($("#carousel-proyects .carousel-inner"));
+                $('<li><img src="img/myproyect2.jpg" alt="Navigation avatar" data-target="#carousel-proyects" data-slide-to="1" class="img-fluid img-circle"></li>').appendTo($("#carousel-proyects .carousel-indicators"));
+
         })
 
     }).fail(function(jqXHR,textStatus,textError){
