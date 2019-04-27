6f9610e7674f6fe000b017d7c330f4c20a006297
Maycol Sanchez Salazar <mayc1030@gmail.com>
2019-04-27 02:01:58 -0500

    test

--- js/ultimatepost/ultimatePostAjax.js
+++ js/ultimatepost/ultimatePostAjax.js
@@ -25,12 +25,10 @@ function getDataCallbackultimateposts(domainurl,categoria){
 
         var objc = JSON.parse(data);
 
-
         $.each(objc, function(key,value){
                 console.log( value.title);
-
+                $('<blockquote class="blockquote contentdrupalnotes"><img src="backend/'+ value.title+'" height="80" width="80" alt="Avatar" class="img-circle"> <p class="h3">'+value.title+'</p><a class="nav-link developments btn-drupalnotes btn-drupalnotes-outline" href="drupalnotes" role="button"><span class="icon-spinner5"></span><small>my</small> Leer Más</a></blockquote>').appendTo($("#carousel-proyects .carousel-inner .carousel-item"));
         })
-
 
     }).fail(function(jqXHR,textStatus,textError){
         alert("Error al realizar la peticion dame");
