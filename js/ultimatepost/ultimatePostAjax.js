659aa410df440e699fc794fe1f20c140a9bac34d
Maycol Sanchez Salazar <mayc1030@gmail.com>
2019-04-27 01:00:46 -0500

    update location

--- js/ultimatepost/ultimatePostAjax.js
+++ js/ultimatepost/ultimatePostAjax.js
@@ -5,18 +5,20 @@ var url_ultimatepost = "https://maycolsanchezsalazar.000webhostapp.com/cms/views
 $( document ).ready(function() {
 
         var categoria = 1;
-        getDataCallbackultimateposts(categoria);
+        var domainurl=window.location;
+        console.log(domainurl);
+        getDataCallbackultimateposts(domainurl,categoria);
 
 });
 
 
 //get_data_callback();
 // peticion ajax enviada como callback
-function getDataCallbackultimateposts(categoria){
+function getDataCallbackultimateposts(domainurl,categoria){
 
 
         $.ajax({
-                data: { id:categoria},
+                data: { locationurl:domainurl,id:categoria},
                 type:"POST",
                 url:url_ultimatepost
         }).done(function(data,textStatus,jqXHR){
