29e29e4e595a3ec5eac75da92933b0ecfa2a6662
Maycol Sanchez Salazar <mayc1030@gmail.com>
2019-04-28 13:40:33 -0500

    test

--- js/landio.other.js
+++ js/landio.other.js
@@ -7,29 +7,7 @@
     });
 
 
-    // Bootstrap JS
-    // @codekit-prepend "bootstrap/util.js";
-    // @codekit-prepend "bootstrap/carousel.js";
-    // @codekit-prepend "bootstrap/collapse.js";
-    // @codekit-prepend "bootstrap/dropdown.js";
-    // @codekit-prepend "bootstrap/modal.js";
 
-    // Waypoints
-    // @codekit-prepend "plugins/jquery.waypoints.js"
-
-    // Placeholders
-    // @codekit-prepend "plugins/jquery.placeholder.js";
-
-    // Video JS
-    // @codekit-prepend "plugins/video.js";
-
-    // Vimeo modal autoplay
-    // @codekit-prepend "plugins/jquery.vimeo.api.js";
-
-    // Donut Chart
-    // @codekit-prepend "plugins/chart.js";
-
-    var url_ultimatepost = "https://maycolsanchezsalazar.000webhostapp.com/cms/views/ajax/ultimatepost.php";
     var domainurl=  $(location).attr("href");
 
     const TIEMPO_INTERVALO_MILESIMAS_SEG = 3000;
@@ -53,10 +31,8 @@
 
         if(posicionActual === $("#carousel-"+ idselector+" .carousel-indicators li").size()) {
             posicionActual = 1;
-            console.log(posicionActual+"---"+$("#carousel-"+ idselector+" .carousel-indicators li").size())
         } else {
             posicionActual++;
-
         }
 
         renderizarImagen();
@@ -108,75 +84,16 @@
     }
 
 
-    // function getstopSlide(){
-    //     $(".carousel-inner .carousel-item .h3").hover(function(){
-    //         clearInterval(intervalo);
-    //     }, function(){
-    //         intervalo = setInterval(changeView, TIEMPO_INTERVALO_MILESIMAS_SEG);
-    //     });
-    // }
-
-    //function getstopSlide(){
         $(".carousel-inner").hover(function(){
             clearInterval(intervalo);
         }, function(){
             intervalo = setInterval(changeView, TIEMPO_INTERVALO_MILESIMAS_SEG);
         });
-   // }
-
-
-    //get_data_callback();
-// peticion ajax enviada como callback
-    function getDataCallbackultimateposts(domainurl,idselector){
-
-        var categoria = 0;
-
-        switch (idselector) {
-            case "proyects":
-                categoria = 1;
-                break;
-            case "contributions":
-                categoria = 2;
-                break;
-            case "developments":
-                categoria = 3;
-                break;
-        }
-
-        $.ajax({
-            data: { locationurl:domainurl,id:categoria},
-            dataType: "json",
-            type:"POST",
-            url:url_ultimatepost
-        }).done(function(data,textStatus,jqXHR){
-            console.log(posicionActual+" en ajax");
-            var objc = JSON.parse(data);
-
-            $.each(objc, function(key,value){
-
-                var indice = key;
-                if ($('#carousel-'+ idselector +' .carousel-inner').find('div').length < objc.length) {
-                    $('<div class="carousel-item"><blockquote class="blockquote"><img src="img/myproyect1.jpg" height="80" width="80" alt="Avatar" class="img-circle"> <p class="h3">'+ value.title +'</p><footer>My Profile</footer></blockquote></div>').appendTo($("#carousel-"+ idselector +" .carousel-inner"));
-                    $('<li><img src="img/myproyect2.jpg" alt="Navigation avatar" data-target="#carousel-'+ idselector +'" data-slide-to="'+ indice +'" class="img-fluid img-circle"></li>').appendTo($("#carousel-"+ idselector +" .carousel-indicators"));
-                    $("#carousel-"+ idselector +" .carousel-inner div:first-child").addClass(" active");
-                }
-
-            });
-
-        }).fail(function(jqXHR,textStatus,textError){
-            alert("Error al realizar la peticion dame");
-        });
-
-    }
-
 
 
     function start() {
-
         hidecarruselproyect();
         $('#control_portafolio .nav-link.proyects').click();
-       // intervalo = setInterval(changeView, TIEMPO_INTERVALO_MILESIMAS_SEG);
-
     }
 
     start();
--- js/ultimatepost/ultimatePostAjax.js
+++ js/ultimatepost/ultimatePostAjax.js
@@ -1,57 +1,47 @@
 
-// // url para llamar la peticion por ajax
-// var url_ultimatepost = "https://maycolsanchezsalazar.000webhostapp.com/cms/views/ajax/ultimatepost.php";
-//
-// $( document ).ready(function() {
-//
-//         var categoria = 1;
-//         var domainurl=  $(location).attr("href");
-//         getDataCallbackultimateposts(domainurl,categoria);
-//
-// });
 
-// var url_ultimatepost = "https://maycolsanchezsalazar.000webhostapp.com/cms/views/ajax/ultimatepost.php";
-//
-//
-// //get_data_callback();
-// // peticion ajax enviada como callback
-// function getDataCallbackultimateposts(domainurl,idselector){
-//
-//     var categoria = 0;
-//
-//     switch (idselector) {
-//         case "proyects":
-//             categoria = 1;
-//             break;
-//         case "contributions":
-//             categoria = 2;
-//             break;
-//         case "developments":
-//             categoria = 3;
-//             break;
-//     }
-//
-//     $.ajax({
-//         data: { locationurl:domainurl,id:categoria},
-//         dataType: "json",
-//         type:"POST",
-//         url:url_ultimatepost
-//     }).done(function(data,textStatus,jqXHR){
-//
-//         var objc = JSON.parse(data);
-//
-//         $.each(objc, function(key,value){
-//             var indice = key;
-//             if ($('#carousel-'+ idselector +' .carousel-inner').find('div').length < objc.length) {
-//                 $('<div class="carousel-item"><blockquote class="blockquote"><img src="img/myproyect1.jpg" height="80" width="80" alt="Avatar" class="img-circle"> <p class="h3">'+ value.title +'</p><footer>My Profile</footer></blockquote></div>').appendTo($("#carousel-"+ idselector +" .carousel-inner"));
-//                 $('<li><img src="img/myproyect2.jpg" alt="Navigation avatar" data-target="#carousel-'+ idselector +'" data-slide-to="'+ indice +'" class="img-fluid img-circle"></li>').appendTo($("#carousel-"+ idselector +" .carousel-indicators"));
-//                 $("#carousel-"+ idselector +" .carousel-inner div:first-child").addClass(" active");
-//             }
-//
-//         });
-//
-//         getstopSlide()
-//     }).fail(function(jqXHR,textStatus,textError){
-//         alert("Error al realizar la peticion dame");
-//     });
-// }
+
+var url_ultimatepost = "https://maycolsanchezsalazar.000webhostapp.com/cms/views/ajax/ultimatepost.php";
+
+//get_data_callback();
+// peticion ajax enviada como callback
+function getDataCallbackultimateposts(domainurl,idselector){
+
+    var categoria = 0;
+
+    switch (idselector) {
+        case "proyects":
+            categoria = 1;
+            break;
+        case "contributions":
+            categoria = 2;
+            break;
+        case "developments":
+            categoria = 3;
+            break;
+    }
+
+    $.ajax({
+        data: { locationurl:domainurl,id:categoria},
+        dataType: "json",
+        type:"POST",
+        url:url_ultimatepost
+    }).done(function(data,textStatus,jqXHR){
+
+        var objc = JSON.parse(data);
+
+        $.each(objc, function(key,value){
+            var indice = key;
+            if ($('#carousel-'+ idselector +' .carousel-inner').find('div').length < objc.length) {
+                $('<div class="carousel-item"><blockquote class="blockquote"><img src="img/myproyect1.jpg" height="80" width="80" alt="Avatar" class="img-circle"> <p class="h3">'+ value.title +'</p><footer>My Profile</footer></blockquote></div>').appendTo($("#carousel-"+ idselector +" .carousel-inner"));
+                $('<li><img src="img/myproyect2.jpg" alt="Navigation avatar" data-target="#carousel-'+ idselector +'" data-slide-to="'+ indice +'" class="img-fluid img-circle"></li>').appendTo($("#carousel-"+ idselector +" .carousel-indicators"));
+                $("#carousel-"+ idselector +" .carousel-inner div:first-child").addClass(" active");
+            }
+
+        });
+
+        getstopSlide()
+    }).fail(function(jqXHR,textStatus,textError){
+        alert("Error al realizar la peticion dame");
+    });
+}
