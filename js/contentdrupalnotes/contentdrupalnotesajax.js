75f326ebf2214bc890cb6a3dbf3f67c539f418b8
Maycol Sanchez Salazar <mayc1030@gmail.com>
2019-04-28 22:27:49 -0500

    testing

--- js/contentdrupalnotes/contentdrupalnotesajax.js
+++ js/contentdrupalnotes/contentdrupalnotesajax.js
@@ -27,50 +27,9 @@ function get_data_callbackcontentdrupalnotes(){
         type:"POST",
         url:url_contentdrupalnotes
     }).done(function(data,textStatus,jqXHR){
-
-        // obtiene la clave lista del json data
-        var lista = data.lista;
-
-       $(".fcontentdrupalnotes").html("");
-
-        $(".date-seccion .info").remove();
-        $(".view-drupalnotes .info").remove();
-        $(".download-drupalnotes .info").remove();
-        $(".bullhorn-drupalnotes .info").remove();
-        $(".author-seccion .info").remove();
-        $(".linkdownload-drupalnotes .info").remove();
-
-
-        // genera el cuerpo de la tabla
-        var cantidad = 0;
+        console.log("hola mundo");
         var objc = JSON.parse(data);
         console.log(objc);
-
-        $.each(objc, function(key,value){
-            if(key == "lista"){
-                $.each(value, function(i,val){
-
-                    $(".date-seccion").append("<div class='info'>El</div>");
-                    $(".view-drupalnotes").append("<div class='info'><i class='fas fa-eye'></i> </div>");
-                    $(".download-drupalnotes").append("<div class='info'><i class='fas fa-download'></i> </div>");
-                    $(".bullhorn-drupalnotes").append("<div class='info'><i class='fas fa-bullhorn'></i> </div>");
-                    $(".author-seccion").append("<div class='info'>Publicado por </div>");
-                    $(".linkdownload-drupalnotes").append("<div class='info'><a href='files/"+val.title+".pdf' target='_blank'><i class='fas fa-cloud-download-alt'></i></a></div>");
-
-
-                    $('<blockquote class="blockquote contentdrupalnotes"><img src="backend/" height="80" width="80" alt="Avatar" class="img-circle"> <p class="h3">'+val.titulo+'</p><footer></footer><a class="nav-link developments btn-drupalnotes btn-drupalnotes-outline" href="drupalnotes?id='+val.nid+'" role="button"><span class="icon-spinner5"></span><small>my</small> Leer Más</a></blockquote>').appendTo($(".fcontentdrupalnotes"));
-
-                })
-            }
-            if(key == "cantidad"){
-                cantidad = value
-            }
-        })
-            // si es necesario actualiza la cantidad de paginas del paginadorcontentdrupalnotes
-            if(cdnpagina==0){
-                creaPaginadorcontentdrupalnotes(cantidad);
-            }
-
 
     }).fail(function(jqXHR,textStatus,textError){
         alert("Error al realizar la peticion dame".textError);
