d84a25c0deb6566453d26729a5ad0e15b7070f98
Maycol Sanchez Salazar <mayc1030@gmail.com>
2019-10-15 22:56:19 -0500

    tested v-4

--- js/loadsubcategory/loadsubcategoryajax.js
+++ js/loadsubcategory/loadsubcategoryajax.js
@@ -7,8 +7,6 @@ $( document ).ready(function() {
     get_data_subcategoty(id_category);
 });
 
-//get_data_callback();
-// peticion ajax enviada como callback
 function get_data_subcategoty(id_category){
 
     $.ajax({
@@ -18,8 +16,8 @@ function get_data_subcategoty(id_category){
         url:url_subcategories
     }).done(function(data,textStatus,jqXHR){
 
-        var objsubcategory = JSON.parse(data);
-
+        let objsubcategory = JSON.parse(data);
+console.log(objsubcategory);
         $.each(objsubcategory, function(key,value){
 
             $('<p class="h3">'+value.tid+'</p><p class="h3">'+value.name+'</p>').appendTo($(".fcontentdrupalnotes"));
