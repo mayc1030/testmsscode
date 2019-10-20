5f8ff1a040d3d9451c8a510cd2724744be1be606
Maycol Sanchez Salazar <mayc1030@gmail.com>
2019-10-20 10:10:31 -0500

    update url list content category

--- js/listcontentcategory/listcontentcategoryajax.js
+++ js/listcontentcategory/listcontentcategoryajax.js
@@ -1,6 +1,6 @@
 
 // url para llamar la peticion por ajax
-var url_listcontentcategory= "https://maycolsanchezsalazar.000webhostapp.com/cms/views/ajax/getAllDrupalNotes.php";
+var url_listcontentcategory= "https://maycolsanchezsalazar.000webhostapp.com/cms/views/ajax/getAllPostforCategory.php";
 
 function action_button_list_content_category(id_c){
     $('.btn_list_content_category').remove();
